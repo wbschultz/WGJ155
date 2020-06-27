15b1f0b21f1fedccdd1265a71e54654feebf1af2
Bharat Raman <bharat.raman@roundedlearning.com>
2020-06-27 12:30:16 -0700

    added hurdle that translates left. Included a prefabs folder

---
+++ Power Generated Ethically/Assets/Scripts/obstacle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstacle : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * speed);
+    }
+}
